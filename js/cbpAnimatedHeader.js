3522fdc3c31a888fe332a7044717313b7c75a061
gpuellestorres <gpuellestorres@gmail.com>
2016-02-18 11:10:24 -0300

    Reserva Creación

--- js/cbpAnimatedHeader.js
+++ js/cbpAnimatedHeader.js
@@ -27,10 +27,8 @@ var cbpAnimatedHeader = (function() {
 	function scrollPage() {
 		var sy = scrollY();
 		if ( sy >= changeHeaderOn ) {
-			classie.add( header, 'navbar-shrink' );
 		}
 		else {
-			classie.remove( header, 'navbar-shrink' );
 		}
 		didScroll = false;
 	}
